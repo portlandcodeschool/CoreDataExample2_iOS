b6022d3a66b625de489a4388161dffc041416f9c
Your Name <erickbennett@Ericks-MacBook-Pro.local>
2015-02-16 10:50:20 -0800

    Initial Commit

---
+++ CoreDataExample2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataExample2
+//
+//  Created by Erick Bennett on 2/16/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
